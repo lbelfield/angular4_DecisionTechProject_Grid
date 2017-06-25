a258e12d6135d714654a121f481da079acbccb04
Luke Belfield <Luke.Belfield@dealogic.com>
2017-06-25 13:31:16 +0100

    Add table. Use interpolation and ngFor to populate deal

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -13,13 +13,28 @@ export class GridComponent {
     private dealsResponse: Array<Deal>;
     private dealService: DealService;
 
+    private about: string;
+    private contractLength: string;
+    private speedUsage: string;
+    private offer: string;
+    private tv: string;
+    private mobile: string;
+    private cost: string;
+
   constructor(dealService: DealService) {
     this.dealService = dealService;
-
     this.dealService.getProducts()
       .subscribe(deals => {
         this.dealObject = deals;
         this.dealsResponse = this.dealObject.deals;
+
+      this.about = "About";
+      this.contractLength = "contract length";
+      this.speedUsage = "speed/usage";
+      this.offer = "Offer";
+      this.tv = "TV";
+      this.mobile = "Mobile";
+      this.cost = "Cost";
       });
   }
 }
